974040c0930c44cdb51dd0a262c75a3ce2cb6ea2
User <user@MB033361.local>
2024-06-07 15:15:09 -0300

    add image set and design the main screen

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by User on 07/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
